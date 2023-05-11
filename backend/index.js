e624bf426b68b612ea7a1120f48b702b57094143
Sabbir <sabbirkhan.kst178@gamil.com>
2023-05-11 21:17:22 +0600

    Create schema for formation of mongoDB and also create end point and api with router

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,14 @@
 const connectToMongo = require("./db");
+const express = require('express')
 
 connectToMongo()
-
-
-const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World! i am here the best programmer in this world')
-})
+//Available routes
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'));
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port at http://localhost:${port}`)

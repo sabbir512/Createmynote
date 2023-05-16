86725e88be1aae8ab6e338ca16ae813dcf681148
Sabbir <sabbirkhan.kst178@gamil.com>
2023-05-16 21:03:10 +0600

    just commit

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -3,18 +3,28 @@ import noteContext from "./noteContext";
 
 const NoteState = (props) => {
   const host = "http://localhost:5000";
-  const notesInitial = [ ]
+  const notesInitial = []
   const [notes, setNotes] = useState(notesInitial)
 
   //Gets All Notes.
   const getNotes = async () => {
     //TODO API Call
-    
+
   }
   //Add a note
   const addNote = async (title, description, tag) => {
     //TODO API Call
-  
+    console.log("Adding a new note")
+    const note = {
+      "_id": "645f709639f2b13ba7e5abe9q9",
+      "user": "645e47c9c5aa23f22d8347443",
+      "title": title,
+      "description": description,
+      "tag": tag,
+      "date": "2023-05-13T11:12:22.270Z",
+      "__v": 0
+    };
+    setNotes(notes.concat(note));
     // setNotes
   }
 
@@ -33,9 +43,9 @@ const NoteState = (props) => {
 
 
     //Logic to edit in client
-    
-    }
-  
+
+  }
+
   return (
     <noteContext.Provider value={{ notes, addNote, deleteNote, editNote, getNotes }}>{props.children}</noteContext.Provider>
   )

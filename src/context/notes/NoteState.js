ea99844b1858dd7abdc1aea44901509592801b17
Sabbir <sabbirkhan.kst178@gamil.com>
2023-05-16 20:54:43 +0600

    i did something wrong here

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -2,99 +2,44 @@ import React, { useState } from "react";
 import noteContext from "./noteContext";
 
 const NoteState = (props) => {
-const notesInitial = [
-    {
-      "_id": "645f709639f2b1ba71e5abe99",
-      "user": "645e47c9c5aa23f22d834744",
-      "title": "My Title",
-      "description": "Hi i am sabbir and this is my first description in this app",
-      "tag": "personal",
-      "date": "2023-05-13T11:12:22.270Z",
-      "__v": 0
-    } ,
-    {
-      "_id": "645f709639f2b1ba7ew5abe99",
-      "user": "645e47c9c5aa23f22d834744",
-      "title": "My Title",
-      "description": "Hi i am sabbir and this is my first description in this app",
-      "tag": "personal",
-      "date": "2023-05-13T11:12:22.270Z",
-      "__v": 0
-    },
-     {
-      "_id": "645f709639f2b1ba7e5qabe99",
-      "user": "645e47c9c5aa23f22d834744",
-      "title": "My Title",
-      "description": "Hi i am sabbir and this is my first description in this app",
-      "tag": "personal",
-      "date": "2023-05-13T11:12:22.270Z",
-      "__v": 0
-    },
-    {
-        "_id": "645f709639f2b1bad7e5abe99",
-        "user": "645e47c9c5aa23f22d834744",
-        "title": "My Title",
-        "description": "Hi i am sabbir and this is my first description in this app",
-        "tag": "personal",
-        "date": "2023-05-13T11:12:22.270Z",
-        "__v": 0
-      },
-      {
-        "_id": "645f709639f2b1bda7e5abe99",
-        "user": "645e47c9c5aa23f22d834744",
-        "title": "My Title",
-        "description": "Hi i am sabbir and this is my first description in this app",
-        "tag": "personal",
-        "date": "2023-05-13T11:12:22.270Z",
-        "__v": 0
-      },
-      {
-        "_id": "645f709639f2b1asba7e5abe99",
-        "user": "645e47c9c5aa23f22d834744",
-        "title": "My Title",
-        "description": "Hi i am sabbir and this is my first description in this app",
-        "tag": "personal",
-        "date": "2023-05-13T11:12:22.270Z",
-        "__v": 0
-      },
-      {
-        "_id": "645f709639f2b13ba7e5abe99",
-        "user": "645e47c9c5aa23f22d834744",
-        "title": "My Title",
-        "description": "Hi i am sabbir and this is my first description in this app",
-        "tag": "personal",
-        "date": "2023-05-13T11:12:22.270Z",
-        "__v": 0
-      }
-  ]
+  const host = "http://localhost:5000";
+  const notesInitial = [ ]
   const [notes, setNotes] = useState(notesInitial)
 
-  //Add a note
-  const addNote = (title, description, tag)=>{
+  //Gets All Notes.
+  const getNotes = async () => {
     //TODO API Call
-    console.log("Adding a new note")
-    const note = {
-      "_id": "645f709639f2b13ba7e5abe9q9",
-      "user": "645e47c9c5aa23f22d8347443",
-      "title": title,
-      "description": description,
-      "tag": tag,
-      "date": "2023-05-13T11:12:22.270Z",
-      "__v": 0
-    };
-   setNotes(notes.concat(note));
+    
   }
+  //Add a note
+  const addNote = async (title, description, tag) => {
+    //TODO API Call
+  
+    // setNotes
+  }
+
   //Delete a note
-  const deleteNote = (id)=>{
-
+  const deleteNote = (id) => {
+    //TODO API Call
+    console.log("deleting the note with id" + id);
+    const newNote = notes.filter((note) => { return note._id !== id })
+    setNotes(newNote);
   }
+
+
   //Edit a note
-  const editNote = (id)=>{
+  const editNote = async (id, title, description, tag) => {
+    // API Call;
 
-  }
-    return (
-        <noteContext.Provider value={{notes, addNote, deleteNote, editNote}}>{props.children}</noteContext.Provider>
-    )
+
+    //Logic to edit in client
+    
+    }
+  
+  return (
+    <noteContext.Provider value={{ notes, addNote, deleteNote, editNote, getNotes }}>{props.children}</noteContext.Provider>
+  )
 }
+
 
 export default NoteState;
